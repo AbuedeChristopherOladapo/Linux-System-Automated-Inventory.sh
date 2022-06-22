cb4b7690a6aa7f3cf89b5ba5f249d92277e7eec6
Oladapo Christopher Abuede <abuedeoladapo@gmail.com>
2022-06-21 22:02:33 -0500

    System-inventory.sh

---
+++ System-inventory.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+##Author : OLADAPO CHRISTOPHER ABUEDE
+
+#Date    : 06 / 20 / 2022.
+
+#Script Status : Original Script.
+
+#Description   : FULL LINUX SYSTEM INVENTARY USING A BASH-SHELL .
+
+
+echo "Good day to whom it may concern"
+
+sleep 4 
+ 
+echo "Hope you having a hot/warm cup of Coffee , while waiting on my Bash-Script.sh doing the magic for you"
+
+ sleep 4
+
+ echo "relax and enjoy this wonderfull IT-Fight"
+
+ sleep 2
+
+ echo "UPTIME"
+ uptime
+
+ SLEEP 2
+
+ echo "LSCPU"
+ lscpu
+
+ sleep 2 
+
+ echo "FREE -H"
+ free -h
+
+ sleep 2
+
+ echo "LSBLK"
+ lsblk
+
+ sleep 2
+
+ echo "UNAME"
+ uname
+
+ sleep 2
+
+ echo "UNAME -R"
+ uname -r
+
+ echo "LAST-REBOOT"
+ last reboot
+
+ sleep 2
+
+ echo "CAT /ETC/*RELEASE"
+ cat /etc/*release 
+
+ sleep 2 
+
+ echo "IFCONGIF"
+ ifconfig
+
+ sleep 2
+
+ echo "LSCPI" 
+ lscpi
+
+ sleep 2
+
+ echo "rpm"
+ rmp
+
+ sleep 2
+
+ echo 'IWCONFIG"
+ iwconfig
+
+ sleep 2
+
+ echo "IWLIST"
+ iwlist
+
+ sleep 2
+
+ echo "ETHTOOL"
+ ethtool
+
+ sleep 2
+
+ echo "DMESG"
+ dmesg
+
+ sleep 2
+
+ echo "KUDZU"
+ kudzu
+
+ sleep 2 
+
+ echo "RUNLEVEL"
+ runlevel
+
+ sleep 2
+
+ echo "HOSTNAME"
+ hostname
+
+ sleep 2
+
+ echo "NETSTAT"
+ netstat
+
+
+
+
+ 
+
